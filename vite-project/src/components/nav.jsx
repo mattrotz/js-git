59f976be0c72ca70f6629632f338fb43b535073c
tomas castrillon <tomas>
2024-09-05 11:02:25 -0500

    se añadio un navbar junto a algunos estilos

---
+++ vite-project/src/components/nav.jsx
@@ -0,0 +1,11 @@
+import './styles/nav.css'
+
+export const NavBar = () => {
+    return (
+        <nav className="menu">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+        </nav>
+);
+}
