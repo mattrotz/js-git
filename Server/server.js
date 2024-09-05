2a5d23577da08f55ce3eebdea21a23741b3e7c25
tomas castrillon <tomas>
2024-09-05 13:47:01 -0500

    se hizo conexion con mongoDB

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
 const corsOptions = {
     origin: ["http://localhost:5173"],
 };
+
+
+
+const mongoURI='mongodb+srv://tomascastrillon8245:Tomi12182006@cluster0.kltpf.mongodb.net/'
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+} )
+.then(() => {
+    console.log('Conectado a MongoDB Atlas');
+  })
+  .catch(err => {
+    console.error('Error de conexión a MongoDB Atlas', err);
+  });
+
+
 
 app.use(cors(corsOptions))
 
